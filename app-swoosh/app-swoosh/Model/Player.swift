64de9d3f112dde3a5c7e6ac00c628e1c2aae6d38
Eric Developer <ea33sports@me.com>
2018-08-10 14:18:43 -0500

    League buttons added

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Eric Andersen on 8/10/18.
+//  Copyright © 2018 Devslopes. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
